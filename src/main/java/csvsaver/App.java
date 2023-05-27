af61c1dbb808760126fe17692e7764755cace0c1
Kiol-Ice <ramadiel2001@gmail.com>
2023-05-27 18:58:46 +0200

    correction pour exec

---
+++ src/main/java/csvsaver/App.java
@@ -0,0 +1,18 @@
+package csvsaver;
+
+import db.DbTest;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DbTest test = new DbTest("loick", "test");
+		
+		System.out.println(test.getUser());
+		
+		
+		// cmd de lancement 
+		// java -jar target/projet-pablo-loick-0.0.1-SNAPSHOT.jar
+	}
+
+}

0cef5b376814d2f0dfa4a528cb827b767764702c
Kiol-Ice <ramadiel2001@gmail.com>
2023-05-27 18:59:38 +0200

    dbTest class

---
+++ src/main/java/db/DbTest.java
@@ -0,0 +1,31 @@
+package db;
+
+public class DbTest {
+
+	
+	private String user;
+	
+	private String password;
+	
+	public DbTest(String user, String password) {
+		super();
+		this.user = user;
+		this.password = password;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}

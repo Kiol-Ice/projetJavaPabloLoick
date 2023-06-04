c492c988e889c4094b412a4733d27c6e709e812d
pablo <pablo.neussaint@telecom-st-etienne.fr>
2023-06-04 17:31:40 +0200

    Gestion BDD

--- src/main/java/csvsaver/App.java
+++ src/main/java/csvsaver/App.java
@@ -1,10 +1,14 @@
 package csvsaver;
-
+import db.Database;
 import java.io.File;
+import java.sql.Timestamp;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 
 public class App {
 
 	public static void main(String[] args) {
+		Database db = new Database();
 		
 		// cmd de lancement 
 		// java -jar target/projet-pablo-loick-0.0.1-SNAPSHOT.jar
@@ -18,11 +22,24 @@ public class App {
 		    	
 		    	System.out.println("File number " + i);
 		        String filePath = "./fileInput/" + file.getName(); // chemin relatif des .csv
+	        	String timestampString = file.getName().split("_")[1].split("\\.")[0];
 		        
 		        CsvReader reader = new CsvReader(filePath); // Instanciation du Reader
 		        if (reader.ReadFile()) // Lecture du fichier
 		        {
-		        	System.out.println(reader.getFileEntries().get(0)); // Affichage de la 1ere ligne du fichier
+		        	String[] data = reader.getFileEntries().get(0).split(","); // récupération de la 1ere ligne du fichier
+	        		Timestamp timestamp = new Timestamp(0);
+	        		Date date = new Date(0);
+		        	
+		        	try {
+		        		timestamp = new Timestamp(new SimpleDateFormat("yyyyMMddHHmmss").parse(timestampString).getTime());
+		        		date = new Date( new SimpleDateFormat("dd-MM-yyyy").parse(data[3]).getTime());
+		        	}
+		        	catch(java.text.ParseException e)
+		        	{
+		        		e.printStackTrace();	
+		        	}
+		        	db.EnvoieDonnee(Integer.parseInt(data[0].replaceAll(" ", "").replaceAll("\"", "")), data[1], data[2], date, data[4].replaceAll(" ", "").replaceAll("\"", ""), data[5], data[6], data[7], Float.valueOf(data[8]), timestamp);
 		        }
 		        
 		        i++;
